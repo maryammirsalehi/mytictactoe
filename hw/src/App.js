f61460df6035358b5becb021c72169fb8cc46532
Maryam MirSalehi <you@example.com>
2020-05-29 23:10:23 +0430

    new API

---
+++ hw/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Button from '@material-ui/core/Button';
+import Game from './components/Game';
+
+
+function App() {
+  return (
+   <Game /> 
+  );
+}
+
+export default App;
